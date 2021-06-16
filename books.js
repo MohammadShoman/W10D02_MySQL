d4a7ab31ef8f66a312be15048be766cff030bca2
MohammadShoman <mohammad.shoman25@gmail.com>
2021-06-16 12:04:14 -0700

    practice done

--- books.js
+++ books.js
@@ -1,11 +1,11 @@
 const db = require("./db");
 
 const addNewBook = (req, res) => {
-    console.log("addNewBook")
-  const { titel, author, published_ar, price } = req.body;
-  const query = `INSERT INTO books (titel,author,published_ar,price) VALUES (?,?,?,?);`;
-  const array = [titel, author, published_ar, price];
-  console.log("hhhhhhhhhhh",titel, author, published_ar, price)
+  console.log("addNewBook");
+  const { title, author, published_at, price } = req.body;
+  const query = `INSERT INTO books (title,author,published_ar,price) VALUES (?,?,?,?);`;
+  const array = [title, author, published_at, price];
+  console.log("hhhhhhhhhhh", title, author, published_at, price);
   db.query(query, array, (err, result) => {
     if (err) throw err;
     console.log(result);
@@ -13,15 +13,73 @@ const addNewBook = (req, res) => {
   });
 };
 
-const getAllBooks=(req,res)=>{
-    const query=`SELECT * FROM BOOKS`
-    db.query(query,(err, result)=>{
-        if (err) throw err;
+const getAllBooks = (req, res) => {
+  const query = `SELECT * FROM books;`;
+  db.query(query, (err, result) => {
+    if (err) throw err;
     console.log(result);
     res.json(result);
-    })
+  });
+};
+
+const updateBookById = (req, res) => {
+  const { title, author, published_at, price } = req.body;
+  const id = req.params.id;
+  const query = `UPDATE books
+SET title=?,author=?,published_at=?,price=?
+WHERE id=${id};`;
+  const array = [title, author, published_at, price];
+  db.query(query, array, (err, result) => {
+    if (err) throw err;
+
+    res.json(result);
+  });
+};
+
+const deleteBookById = (req, res) => {
+  const id = req.params.id;
+  const query = `DELETE FROM books
+  WHERE id=${id};`;
+
+  db.query(query, (err, result) => {
+    if (err) throw err;
+    console.log(err);
+    res.json(result);
+  });
+};
+
+const orderBy = (req, res) => {
+  const query = `SELECT * FROM books ORDER BY price DESC ;`;
+  db.query(query, (err, result) => {
+    if (err) throw err;
+    res.json(result);
+  });
+};
+
+const getBooksPriceNull = (req, res) => {
+  const query = `SELECT * FROM books
+  WHERE price IS NULL;`;
+  db.query(query, (err, result) => {
+    if (err) throw err;
+    res.json(result);
+  });
+};
+
+const getBooksPriceNotNull=(req,res)=>{
+  const query = `SELECT * FROM books
+  WHERE price IS NOT NULL;`;
+  db.query(query, (err, result) => {
+    if (err) throw err;
+    res.json(result);
+  });
 }
+
 module.exports = {
   addNewBook,
-  getAllBooks
+  getAllBooks,
+  updateBookById,
+  deleteBookById,
+  orderBy,
+  getBooksPriceNull,
+  getBooksPriceNotNull
 };
--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.use(express.json());
 
 app.get('/books',books.getAllBooks)
 app.post("/books",books.addNewBook)
+app.put('/books/:id',books.updateBookById)
+app.delete('/books/:id',books.deleteBookById)
+app.get('/books/order-by',books.orderBy)
+app.get('/books/is-null',books.getBooksPriceNull)
+app.get('/books/not-null',books.getBooksPriceNotNull)
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
